0483880800df8142a088644d2dd65bff4be7635d
swaghay <upabhay@gmail.com>
2023-12-01 13:31:30 -0600

    abhay

---
+++ _build/default/lib/procaml.ml
@@ -0,0 +1,28 @@
+include Ast
+
+module Parser = Parser
+module Lexer = Lexer
+
+let parse (s : string) : expression =
+  let lexbuf = Lexing.from_string s in
+  let ast = Parser.expression_eof Lexer.token lexbuf in
+     ast
+
+let string_of_declaration = String_of.string_of_declaration
+  
+let rec prover rules declarations =
+      match declarations with
+         | ProofDeclaration (nm, vars, Equality (lhs,rhs), None) :: rest
+            -> (* no hint, so let's prove this *)
+               Prove.prove rules lhs rhs nm :: prover ((List.map typedVariableVariable vars,nm,lhs,rhs)::rules) rest
+         | ProofDeclaration (nm, vars, Equality (lhs,rhs), _) :: rest
+            -> (* we got a hint so we simply assume the statement *)
+               prover ((List.map typedVariableVariable vars,nm,lhs,rhs)::rules) rest
+         | _ :: rest -> prover rules rest
+         | [] -> []
+
+let prover_main decls =
+      prover [] decls |>
+      List.map (String.concat "\n") |>
+      String.concat "\n\n" |>
+      print_endline
